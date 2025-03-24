ba2165132cabf6c13cf2aa3c128e6157a9d1aba1
Shin Juheon <juheon8794@gmail.com>
2025-03-24 14:28:53 +0900

    4주차 원의 면적 계산하는 프로그램 계발 완료

---
+++ Helloworld/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double round = 10.0;
+        double PI = 3.14;
+        double result= round * round * PI;
+        System.out.println(result);
+    }
+}
