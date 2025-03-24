e65c5befe51533ce72af415c90227dd6fd73423b
Shin Juheon <juheon8794@gmail.com>
2025-03-24 14:46:07 +0900

    실습과제 2 코드 수정

--- Helloworld/src/Main.java
+++ Helloworld/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         double round = 10.0;
-        double PI = 3.14;
+        final double PI = 3.14;
         double result= round * round * PI;
-        System.out.println(result);
+        System.out.println("반지름이 " + round + "인 원의 면적은 " + result + "이다");
     }
 }
