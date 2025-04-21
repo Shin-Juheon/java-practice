ccc51ac1eb16c6e190e22f45eb39b69e251ba954
Shin Juheon <juheon8794@gmail.com>
2025-04-21 14:06:24 +0900

    중간고사 준비

---
+++ XOR/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        System.out.println("입력: ");
+        num = sc.nextInt();
+
+        if (1<=num && num<=100) {
+            System.out.println("1에서 100사이의 값입니다.");
+        } else {
+            System.out.println("1에서 100사이의 값이 아닙니다.");
+        }
+
+    }
+}
