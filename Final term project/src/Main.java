49dbd29157a4bbdb615c4207d3851de4d811f475
Shin Juheon <juheon8794@gmail.com>
2025-04-24 11:39:09 +0900

    기말고사 준비

---
+++ Final term project/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static ArrayList<String> memo = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+
+    }
+}
