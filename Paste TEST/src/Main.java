8710794ac8c606f1dc14ec1580c2442710b7cbbf
Shin Juheon <juheon8794@gmail.com>
2025-04-24 11:32:14 +0900

    잘라내기 테스트

---
+++ Paste TEST/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String product;
+        int price = 0;
+
+        System.out.println("물건을 입력하시오: ");
+        product = sc.nextLine();
+
+        System.out.println("가격을 입력하시오: ");
+        price = sc.nextInt();
+
+        System.out.println(product + "는" + price + "원입니다.");
+    }
+}
