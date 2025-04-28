f79deca0a3ad823f51eda39df7239d0f95c849ec
Shin Juheon <juheon8794@gmail.com>
2025-04-28 12:33:12 +0900

    기말고사 준비

--- Final term project/src/Main.java
+++ Final term project/src/Main.java
@@ -2,11 +2,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    // 할일 저장 리스트
     public static ArrayList<String> memo = new ArrayList<>();
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int choice;
+        Scanner sc = new Scanner(System.in); //사용자 입력 받기
+        int choice; // 선택값 저장 변수
 
 
     }
