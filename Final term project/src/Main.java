94b85edde247a0fd6e813f9bc67c6a7abdb44a51
Shin Juheon <juheon8794@gmail.com>
2025-05-02 13:05:34 +0900

    기말고사 수정

--- Final term project/src/Main.java
+++ Final term project/src/Main.java
@@ -3,12 +3,21 @@ import java.util.Scanner;
 
 public class Main {
     // 할일 저장 리스트
-    public static ArrayList<String> memo = new ArrayList<>();
+    private static ArrayList<String> memo = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); //사용자 입력 받기
         int choice; // 선택값 저장 변수
 
+        do {
+            // 메뉴 표시
+            System.out.print("\n--- 할 일 관리 ---");
+            System.out.println("1. 할 일 추가");
+            System.out.println("2. 할 일 삭제");
+            System.out.println("3. 할 일 보기");
+            System.out.println("4. 종료");
+            System.out.println("메뉴 선택: ");
 
+        }
     }
 }
