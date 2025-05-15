0b8cec8aa13a3fa1305cd19c3541e93bace88fee
Shin Juheon <juheon8794@gmail.com>
2025-05-15 16:44:24 +0900

    작성

--- Final Term Project/src/Main.java
+++ Final Term Project/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
+    static ArrayList<String> memo = new ArrayList<>();
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.print(
+                    """                                                   
+                    --- 
+                            """
+            )
+        }
+
     }
 }
